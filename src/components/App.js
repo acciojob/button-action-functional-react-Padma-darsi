6cfe17543e0cbbb3aa5b9025ecfd4c15b0ea6941
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-11-06 02:44:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
-import React, {Component, useState} from "react";
+import React, { useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  // State to track whether paragraph should be shown
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  // Handler for button click
+  const handleClick = () => {
+    setShowParagraph(true);
+  };
+
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* Button with id="click" */}
+      <button id="click" onClick={handleClick}>
+        Click Me
+      </button>
+
+      {/* Conditionally render paragraph when showParagraph is true */}
+      {showParagraph && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.
+        </p>
+      )}
     </div>
   );
 }
-
 
 export default App;
